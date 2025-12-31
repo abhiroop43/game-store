52af88ef00a322c6340d099dd33c521338c61f83
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-12-31 08:54:13 +0400

    feat(games): enhance game management functionality with CRUD support
    * Added support for editing games via `EditGame` component, including dynamic title updates and improved validation handling.
    * Integrated game deletion and update methods in `GamesClient`.
    * Updated Home component to display action icons for editing and deleting games.
    * Included Bootstrap icons for UI improvements.

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -32,13 +32,16 @@ public class GamesClient
         },
     ];
 
-    private readonly Genre[] genres = new GenresClient().GetGenres();
+    private readonly Genre[] _genres = new GenresClient().GetGenres();
 
-    public GameSummary[] GetGames() => [.. _games];
+    public GameSummary[] GetGames()
+    {
+        return [.. _games];
+    }
 
     public void AddGame(GameDetails game)
     {
-        var genre = genres.FirstOrDefault(g => g.Id == game.GenreId);
+        var genre = _genres.FirstOrDefault(g => g.Id == game.GenreId);
         var gameSummary = new GameSummary
         {
             Id = _games.Count + 1,
@@ -50,16 +53,37 @@ public class GamesClient
         _games.Add(gameSummary);
     }
 
+    public void UpdateGame(GameDetails game)
+    {
+        var genre = _genres.FirstOrDefault(g => g.Id == game.GenreId);
+        var existingGame = _games.Find(g => g.Id == game.Id);
+        if (existingGame is null)
+            return;
+
+        existingGame.Name = game.Name;
+        existingGame.Genre = genre != null ? genre.Name : "Unknown";
+        existingGame.Price = game.Price;
+        existingGame.ReleaseDate = game.ReleaseDate;
+    }
+
+    public void DeleteGame(int id)
+    {
+        var existingGame = _games.Find(g => g.Id == id);
+
+        if (existingGame is null)
+            return;
+
+        _games.Remove(existingGame);
+    }
+
     public GameDetails GetGameById(int id)
     {
         var game = _games.Find(g => g.Id == id);
 
         if (game == null)
-        {
             return null!;
-        }
 
-        var genre = genres.FirstOrDefault(g =>
+        var genre = _genres.FirstOrDefault(g =>
             string.Equals(g.Name, game.Genre, StringComparison.OrdinalIgnoreCase)
         );
 
